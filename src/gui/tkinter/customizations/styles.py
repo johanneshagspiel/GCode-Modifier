ed5ecbfb92c2a0c394f8190eef46c0f26a21ef20
jhagspiel <j.hagspiel@student.tudelft.nl>
2020-11-27 19:13:09 +0100

    Deleted the old tkinter mainscreen

--- src/gui/tkinter/customizations/styles.py
+++
@@ -1,8 +0,0 @@
-from tkinter import W, CENTER
-
-class Styles():
-    label_style_heading = {"font": ("Eurostile LT Std bold", 18), "background": "white", "anchor": CENTER}
-    label_style_text = {"font": ("Eurostile LT Std", 18), "background": "white", "anchor": W}
-    checkbox_style = {"font": ("Eurostile LT Std", 18), "background": "white", "anchor": W}
-    entry_style = {"font": ("Eurostile LT Std", 18), "background": "white"}
-    button_style = {"font": ("Eurostile LT Std bold", 18), "background": "white", "anchor": CENTER}
